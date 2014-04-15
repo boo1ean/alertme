d12d5bbd0f9b6d7dbcf5d32582dfa865fe69566d
boo1ean <boo1ean0807@gmail.com>
2014-04-15 23:20:25 +0300

    Implement snapshots fetching methods

--- app/storage.js
+++ app/storage.js
@@ -1,16 +1,47 @@
 var fs = require('q-io/fs');
+var Q = require('q');
 
 var storagePath = __dirname + '/../storage/'
 var resolvePath = function(filename) {
 	return storagePath + filename;
 };
 
+var totalRecords = 0;
+
 var storage = {
 	save: function(snapshot) {
-		var now = new Date().getTime();
-		var filePath = resolvePath(now);
-
+		var filePath = resolvePath(totalRecords++);
 		return fs.write(filePath, JSON.stringify(snapshot));
+	},
+
+	get: function(index) {
+		var filePath = resolvePath(index);
+		return fs.read(filePath).then(function(out) {
+			return JSON.parse(out);
+		});
+	},
+
+	last: function() {
+		return storage.get(totalRecords - 1);
+	},
+
+	lastTwo: function() {
+		if (totalRecords < 2) {
+			throw new Error('Not enough snapshots');
+		}
+
+		var deferred = Q.defer();
+
+		Q.all([
+			storage.get(totalRecords - 2),
+			storage.get(totalRecords - 1)
+		]).then(function(results) {
+			deferred.resolve(results[0], results[1]);
+		}, function() {
+			deferred.reject(arguments);
+		});
+
+		return deferred.promise;
 	}
 };
 
