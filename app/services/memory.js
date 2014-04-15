92ea81fe6cb8cd46315d96ee796fe6bd6f523568
boo1ean <boo1ean0807@gmail.com>
2014-04-15 22:31:53 +0300

    Implement basic memory service

---
+++ app/services/memory.js
@@ -0,0 +1,43 @@
+var exec = require('../exec');
+
+var command = '/usr/bin/free -tmo | /usr/bin/tail -n+2 | /usr/bin/awk \'{print $2","$3-$6-$7","$4+$6+$7}\'';
+
+var numerify = function(n) {
+	return n - 0;
+};
+
+
+var service = {
+	stats: function() {
+		return exec(command).then(function(out) {
+			out = out.split('\n').filter(Boolean).map(function(row) {
+				return row.split(',').map(numerify);
+			});
+
+			var data = {
+				memory: {
+					total: out[0][0],
+					used: out[0][1],
+					free: out[0][2]
+				},
+
+				swap: {
+					total: out[1][0],
+					used: out[1][1],
+					free: out[1][2]
+				},
+
+				total: {
+					total: out[2][0],
+					used: out[2][1],
+					free: out[2][2]
+				}
+			};
+
+			return data;
+		});
+	}
+};
+
+
+module.exports = service;
