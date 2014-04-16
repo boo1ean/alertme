3a3135d3c3b29704408aa6b471bd1b15b055f1df
boo1ean <boo1ean0807@gmail.com>
2014-04-16 09:08:26 +0300

    Move stats gathering function to services/index

--- app/services/index.js
+++ app/services/index.js
@@ -1,4 +1,22 @@
-module.exports = {
+var Q = require('q');
+
+var services = {
 	memory: require('./memory'),
 	cpu: require('./cpu')
+};
+
+module.exports = function() {
+	var data = {};
+
+	return Q.all(_.map(services, function(service, name) {
+
+		return service.stats().then(function(result) {
+			data[name] = result;
+		});
+
+	})).then(function() {
+
+		return data;
+
+	});
 };
