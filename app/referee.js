f4a11e1d189574b0404fa03fd6acd46284c63039
boo1ean <boo1ean0807@gmail.com>
2014-04-16 09:24:59 +0300

    Trigger event on bus if condtition is true

--- app/referee.js
+++ app/referee.js
@@ -1,6 +1,12 @@
+var bus = require('./bus');
+
 var referee = {
 	judge: function(events, it) {
-		console.log(events, it);
+		for (var i in events) {
+			if (eval(events[i].condition)) {
+				bus.emit(events[i].name, it);
+			}
+		}
 	}
 };
 
